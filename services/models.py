7b275b47aae7427077b93e8ab1316a82bb1fa0cd
AriasPaez <ariaspaez.dev@gmail.com>
2020-10-12 16:17:26 -0500

    [feat]Model services

--- services/models.py
+++ services/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Service(models.Model):
-    # _id=models.ObjectIdField()
     name=models.CharField(max_length=50,null=False)
-    description=models.CharField(max_length=200,null=True)
-    image=models.ImageField(upload_to='services')
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
+    description=models.CharField(max_length=200,null=True,blank=True)
+    image = models.ImageField(upload_to='service',default='../static/services/service_default.png', null=True)
+    data_creation=models.DateTimeField(auto_now_add=True)
+    data_update=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name='Sevicio'
+        verbose_name_plural='Servicios'
+    
+    def __str__(self):
+        return self.name
--- webStoreApp/models.py
+++ webStoreApp/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    name=models.CharField(max_length=50,null=False)
-    brand=models.CharField(max_length=50,null=False)
-    price=models.IntegerField(null=False)
-    image = models.ImageField(upload_to='store')
-    created=models.DateTimeField(auto_now_add=True)
+# class Item(models.Model):
+#     name=models.CharField(max_length=50,null=False)
+#     description=models.CharField(max_length=200,null=True)
+#     purchase_price=models.IntegerField(null=False)
+#     sale_price=models.IntegerField(null=False)
+#     image = models.ImageField(upload_to='store',default='../static/webStoreApp/default_item.png')
+#     stock=models.IntegerField(null=False)
+#     data_creation=models.DateTimeField(auto_now_add=True)
+#     data_update=models.DateTimeField(auto_now_add=True)
+#     fk_brand=
+    
+    
